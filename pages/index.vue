98727ec58503bf60d905c30755bf4c41ec3e32e1
Kubtuntha <buriramit@gmail.com>
2018-05-03 13:33:07 +0700

    First-demo

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <img src="/LOGO_VEC_OK.png" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" />
+      </div>
+      <v-card>
+        <v-card-title class="headline">--ระบบจัดการข้อมูลนักเรียน นักศึกษา--</v-card-title>
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-text-field
+              label="E-mail"
+              v-model="email"
+              :rules="emailRules"
+              required
+            ></v-text-field>
+            <v-text-field
+                name="input-10-1"
+                label="Enter your password"
+                hint="At least 8 characters"
+                v-model="password"
+                min="8"
+                :append-icon="e1 ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (e1 = !e1)"
+                :type="e1 ? 'password' : 'text'"
+                counter
+            ></v-text-field>
+         </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  export default {
+    data: () => ({
+      password: '',
+      e1: false,
+      valid: false,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ]
+    })
+  }
+</script>
